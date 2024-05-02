e88b312622d60157c02b3674c89e197975db8da7
Ismikoslomet <143516585+Ismikoslomet@users.noreply.github.com>
2024-05-02 11:12:13 +0200

    The code

---
+++ src/test/java/oslomet/webprog/oblig3kinonr2/Oblig3KinoNr2ApplicationTests.java
@@ -0,0 +1,13 @@
+package oslomet.webprog.oblig3kinonr2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig3KinoNr2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
